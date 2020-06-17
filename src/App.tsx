9600f08727b1576a46b92357a5a6c3843dab19a9
Giovani  Jose <giovanijose@Mac-mini-de-Giovani.local>
2020-06-17 19:43:50 -0300

    adds finally config splash screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Text } from 'react-native';
+import SplashScreen from 'react-native-splash-screen';
 
 const App: React.FC = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
   return <Text>aaa</Text>;
 };
 
